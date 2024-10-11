e1bec89c9d7b2c1df7ff513cd2923ae8c984cca6
NyLaurent <laulanyumbayire@gmail.com>
2024-10-11 09:18:03 -0600

    about section created

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Hero from './components/Hero'
-
+import About from './components/About'
 function page() {
   return (
     <div>
       <Hero></Hero>
+      <About></About>
     </div>
   )
 }
