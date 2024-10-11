f54d0d35bfd08e44d576eacf46456602baf20683
NyLaurent <laulanyumbayire@gmail.com>
2024-10-11 09:54:23 -0600

    Features section created

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from './components/Hero'
 import About from './components/About'
+import Features from './components/Features'
 function page() {
   return (
     <div>
       <Hero></Hero>
       <About></About>
+      <Features></Features>
     </div>
   )
 }
