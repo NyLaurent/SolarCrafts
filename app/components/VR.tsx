ed21de03fe6e0f6ad206e6ce29b55c966727c83d
NyLaurent <laulanyumbayire@gmail.com>
2024-10-11 16:22:19 -0600

    fnished VR section

--- app/components/VR.tsx
+++ app/components/VR.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import Image from 'next/image'
+import Virtual from '../assets/VR.png'
 
 function VR() {
   return (
-    <div>VR</div>
+    <div className='flex flex-col  lg:flex-row justify-between px-6 lg:px-9 pt-9'>
+        <div className='lg:w-1/2'>
+            <h1 className='text-lg font-bold text-[#294680]'>The Planets Virtual Reality</h1>
+            <br /><br />
+            <p className='text-sm w-full lg:w-[80%]'>
+              Get a stunning experience of using our virtual reality feature in our mobile apps which is easily accessible for everyone.
+            </p>
+        </div>
+        
+        <div className='lg:w-1/2 flex justify-center mt-4 lg:mt-0'>
+            <Image src={Virtual} alt='' className=''></Image>
+        </div>
+    </div>
   )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Hero from './components/Hero'
 import About from './components/About'
 import Features from './components/Features'
+import VR from './components/VR'
 function page() {
   return (
     <div>
       <Hero></Hero>
       <About></About>
       <Features></Features>
+      <VR></VR>
     </div>
   )
 }
