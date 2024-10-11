e53a996472e1890f873495b518850f7cf7ead9d9
NyLaurent <laulanyumbayire@gmail.com>
2024-10-11 09:04:32 -0600

    finished hero section and responsiveness achieves

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,23 +1,35 @@
 import React from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
+import HeroImage from '../assets/Hero.png'
 
 function Hero() {
   return (
-    <div className='pt-20 px-9'>
-      <div>
-        <h1 className='text-[30px]'>Explore and Learn <br />
-        More About our Solar System</h1>
+    <div className='pt-20 px-9 flex flex-col md:flex-row'>
+      <div className='md:w-1/2'>
+        <h1 className='text-[24px] md:text-[30px]'>
+          Explore and Learn <br />
+          More About our Solar System
+        </h1>
 
-        <p className='w-[45%] pt-9 text-sm text-[#696984]'>
-        Through using solarCrafts you are able to learn more things about solar systems through getting many information about it and also through the use of our virtual reality feature where you are able to view how it is in space.
+        <p className='w-full md:w-[65%] pt-6 md:pt-9 text-sm text-[#696984]'>
+          Through using solarCrafts you are able to learn more things about solar systems through getting many information about it and also through the use of our virtual reality feature where you are able to view how it is in space.
         </p>
-        <div className='flex flex-row gap-14 pt-9'>
-          <button className='w-[150px] h-[40px] bg-[#294680] rounded-sm'>
+
+        {/* Responsive button and link section */}
+        <div className='flex flex-col items-center md:flex-row md:items-start gap-4 md:gap-14 pt-6 md:pt-9'>
+          <button className='w-[200px] h-[45px] bg-[#294680] text-white rounded-md'>
             Get Started Now
           </button>
-          <p className='pt-1'>Or</p>
-          <Link href={'/'} className='text-[#294680] pt-1'>Download our mobile app</Link>
+          <p className='pt-2 md:pt-1 text-sm text-center md:text-left'>Or</p>
+          <Link href={'/'} className='text-[#294680] pt-2 md:pt-1 text-sm text-center md:text-left'>
+            Download our mobile app
+          </Link>
         </div>
+      </div>
+
+      <div className='flex justify-center md:justify-end mt-8 md:mt-0 md:w-1/2'>
+        <Image src={HeroImage} alt='' className='w-[300px] h-[240px] md:w-[450px] md:h-[480px]'></Image>
       </div>
     </div>
   )
