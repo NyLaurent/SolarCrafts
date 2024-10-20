ec5e61b5b7052b12297964e8a019c6de4f3495f3
NyLaurent <laulanyumbayire@gmail.com>
2024-10-19 20:59:18 -0600

    updated

--- app/components/Testimonials.tsx
+++ app/components/Testimonials.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import testimonial from '../assets/testimonial.png'
 
 function Testimonials() {
   return (
--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,16 @@ import Hero from './components/Hero'
 import About from './components/About'
 import Features from './components/Features'
 import VR from './components/VR'
+import Testimonials from './components/Testimonials'
 function page() {
   return (
     <div>
       <Hero></Hero>
       <About></About>
       <Features></Features>
+      
       <VR></VR>
+      <Testimonials></Testimonials>
     </div>
   )
 }
