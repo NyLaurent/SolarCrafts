703aae0dba64f83a53a9f5bf5ab2129046449f9f
NyLaurent <laulanyumbayire@gmail.com>
2024-10-21 15:54:36 -0600

    responsiveness

--- app/components/Testimonials.tsx
+++ app/components/Testimonials.tsx
@@ -4,20 +4,20 @@ import testimonial from '../../assets/testimonial.png'
 import Testimonial from './Testimonial'
 function Testimonials() {
   return (
-    <div className='flex flex-row justify-between px-7'>
+    <div className='flex flex-row justify-between px-7 pt-9'>
 <div>
 <div className='flex flex-row gap-4'>
             <div>
             <Image src={testimonial} alt="" />
             </div>
 
-            <h1 className='font-bold text-lg relative bottom-3'>Testimonials</h1>
+            <h1 className='font-bold text-lg relative bottom-3 pb-3'>Testimonials</h1>
         </div>
-        <div className=' flex flex-col gap-3'>
+        <div className=' flex flex-col gap-6'>
             <h1 className='text-2xl text-[#294680] font-bold'>What They Say?</h1>
-            <p className='w-[60%] text-sm text-[#696984]'>Solarcraft has got more than 100k positive ratings from our users around the world. </p>
-            <p className='w-[60%] text-sm text-[#696984]'>Some of the students and teachers were greatly helped by the Solarcraft</p>
-            <p className='w-[60%] text-sm text-[#696984]'>Are you too? Please give your assessment</p>
+            <p className='w-[80%] text-[#696984]'>Solarcraft has got more than 100k positive ratings from our users around the world. </p>
+            <p className='w-[80%] text-md text-[#696984]'>Some of the students and teachers were greatly helped by the Solarcraft</p>
+            <p className='w-[80%] text-md text-[#696984]'>Are you too? Please give your assessment</p>
         </div>
 </div>
 <div>
